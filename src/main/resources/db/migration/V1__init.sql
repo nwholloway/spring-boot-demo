28fa68e8036f7240a8ce36829e740643ff99da86
Nick Holloway <nick.holloway@pyrites.org.uk>
2024-07-08 18:53:34 +0100

    Initial commit

---
+++ src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,4 @@
+create table demo
+(
+    id bigserial
+);
---
+++ src/test/java/com/github/nwholloway/TestApplication.java
@@ -0,0 +1,10 @@
+package com.github.nwholloway;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(Application::main).with(TestcontainersConfiguration.class).run(args);
+	}
+}
